017af08af4856bfef1fa129a134aca1b8dc12e98
drj613 <drj613@gmail.com>
2017-11-14 20:50:50 -0800

    Mixed up responses from server listen with request to server. Oops!

--- twoServers.js
+++ twoServers.js
@@ -4,10 +4,35 @@ var NICEPORT = 7000;
 var MEANPORT = 7500;
 
 function kindRequest(request, response){
-    response.end('Serving a response from the kind server');
+    let num = Math.floor(Math.random()*5);
+    
+        if(num === 0){
+            response.end("You have a beautiful smile.");
+        } else if (num === 1) {
+            response.end("Your farts smell like roses.");
+        } else if (num === 2) {
+            response.end("The back of your head is ridiculous.");
+        } else if (num === 3) {
+            response.end("You literally CAN even.");
+        } else {
+            response.end("You're alright I guess.");
+        }
+    
 }
 function rudeRequest(request, response){
-    response.end('Serving a response from the rude server');
+    let num = Math.floor(Math.random()*5);
+    
+        if(num === 0){
+            response.end("You have an ugly laugh.");
+        } else if (num === 1) {
+            response.end("You should smile a lot less. It makes the rest of us uncomfortable.");
+        } else if (num === 2) {
+            response.end("People talk about you behind your back. Like, in a bad way.");
+        } else if (num === 3) {
+            response.end("Nah, too easy of a target.");
+        } else {
+            response.end("You're SO brave for wearing that.");
+        }
 }
 
 
@@ -15,33 +40,9 @@ var niceServer = http.createServer(kindRequest);
 var rudeServer = http.createServer(rudeRequest);
 
 niceServer.listen(NICEPORT, function(){
-    let num = Math.floor(Math.random()*5);
-
-    if(num === 0){
-        console.log("You have a beautiful smile.");
-    } else if (num === 1) {
-        console.log("Your farts smell like roses.");
-    } else if (num === 2) {
-        console.log("The back of your head is ridiculous.");
-    } else if (num === 3) {
-        console.log("You literally CAN even.");
-    } else {
-        console.log("You're alright I guess.");
-    }
+    console.log('Serving a response from the kind server');
 });
 
 rudeServer.listen(MEANPORT, function(){
-    let num = Math.floor(Math.random()*5);
-
-    if(num === 0){
-        console.log("You have an ugly laugh.");
-    } else if (num === 1) {
-        console.log("You should smile a lot less. It makes the rest of us uncomfortable.");
-    } else if (num === 2) {
-        console.log("People talk about you behind your back. Like, in a bad way.");
-    } else if (num === 3) {
-        console.log("Nah, too easy of a target.");
-    } else {
-        console.log("You're SO brave for wearing that.");
-    }
+    console.log('Serving a response from the rude server');
 });
